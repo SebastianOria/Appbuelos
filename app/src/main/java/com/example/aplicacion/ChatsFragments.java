645c64624bce739ab42467434305fbb4f572ace5
SebastianOria <sebastian22oria@gmail.com>
2022-06-15 17:56:08 -0400

    avance

--- app/src/main/java/com/example/aplicacion/ChatsFragments.java
+++ app/src/main/java/com/example/aplicacion/ChatsFragments.java
@@ -2,63 +2,63 @@ package com.example.aplicacion;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatsFragments#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ChatsFragments extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View ChatViewUnica;
+    private RecyclerView ChatLista;
+    private DatabaseReference Contactos;
+    private FirebaseAuth auth;
+    private String CurrentUserId;
 
     public ChatsFragments() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatsFragments.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatsFragments newInstance(String param1, String param2) {
-        ChatsFragments fragment = new ChatsFragments();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats_fragments, container, false);
+        auth = FirebaseAuth.getInstance();
+        CurrentUserId = auth.getCurrentUser().getUid();
+        Contactos = FirebaseDatabase.getInstance().getReference().child(CurrentUserId);
+        ChatViewUnica = inflater.inflate(R.layout.fragment_chats_fragments, container, false);
+        ChatLista = (RecyclerView) ChatViewUnica.findViewById(R.id.chat_lista);
+        ChatLista.setLayoutManager(new LinearLayoutManager(getContext()));
+        return ChatViewUnica;
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+    }
+
+    public static class ChatsViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView imagenc;
+        TextView nombrec;
+
+
+        public ChatsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagenc = itemView.findViewById(R.id.imagen_contacto);
+            nombrec = itemView.findViewById(R.id.nombre_contacto);
+        }
+    }
+
 }
